35f3d871fa3bfca792644bd18d5e390415163ead
Julian Roman <romanapto@gmail.com>
2024-02-28 04:09:05 -0500

    Fin seccion 3 -dise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagesfoundComponent,
+    DashboardComponent,
+    BreadcrumsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    PagesComponent,
+    ProgressComponent,
+    Grafica1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
